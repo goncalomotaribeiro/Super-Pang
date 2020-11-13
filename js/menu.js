0f39d1da31097ccf3542c19b3abd32210ceb318d
goncalomotaribeiro <goncalomota.cr@gmail.com>
2020-11-13 15:00:23 +0000

    ff

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+const svg = document.querySelector('#svg');
+
+let btn1Jogador = document.querySelector('#btn1Jogador')
+
+btn1Jogador.addEventListener('click', () =>{
+    window.location.href='../index.html'
+    console.log('oi');
+})
